f45d7e98518bb1b9b070625fd93ea944c9b024f1
Maciek Bajor <maciekbajor@gmail.com>
2026-07-18 21:41:35 +0200

    Update nvim-raccoon plugin source

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -59,7 +59,7 @@ Plug('3rd/image.nvim')
 Plug('3rd/diagram.nvim')
 
 -- PR Review
-Plug('~/.vim/plugged/nvim-raccoon')
+Plug('bajor/nvim-raccoon')
 Plug('bajor/nvim-raccoon-diffs')
 
 vim.call('plug#end')
